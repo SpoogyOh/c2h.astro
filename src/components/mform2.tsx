e9dab8fb4e13ea3a6f7fcbd7703ac12863ee8546
SpoogyOH <opiola.daniel@gmail.com>
2022-10-27 19:42:59 +0200

    modified:   package-lock.json 	modified:   package.json 	new file:   public/daniel.png 	new file:   public/inits.png 	new file:   public/logo.png 	new file:   public/uni.png 	new file:   src/components/Maillinglist.astro 	new file:   src/components/Team.astro 	new file:   src/components/mform2.tsx 	modified:   src/pages/index.astro

---
+++ src/components/mform2.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { useForm } from "react-hook-form";
+
+export default function MForm2() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+  const onSubmit = (data) => console.log(data);
+  console.log(errors);
+
+  return (
+    <div className="">
+      <form onSubmit={handleSubmit(onSubmit)} className="space-x-2">
+        <input
+          type="text"
+          placeholder="E-Mail"
+          {...register("Email", { required: true, pattern: /^\S+@\S+$/i })}
+          className="border-2 rounded-lg h-12 w-72 text-center"
+        />
+
+        <button
+          type="submit"
+          className=" rounded-lg bg-black text-white h-12 w-36"
+        >
+          Probiere es aus
+        </button>
+      </form>
+    </div>
+  );
+}
