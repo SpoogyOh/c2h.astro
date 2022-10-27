e56f285aaff3012db2ae795e19db4861a6040e59
SpoogyOH <opiola.daniel@gmail.com>
2022-10-27 19:25:44 +0200

    new file:   .gitignore 	new file:   .vscode/extensions.json 	new file:   .vscode/launch.json 	new file:   README.md 	new file:   astro.config.mjs 	new file:   package-lock.json 	new file:   package.json 	new file:   public/favicon.svg 	new file:   public/monitor1.png 	new file:   public/monitor2.png 	new file:   public/monitor3.png 	new file:   src/components/Card.astro 	new file:   src/components/Hero.astro 	new file:   src/components/Magic.astro 	new file:   src/components/mform.tsx 	new file:   src/env.d.ts 	new file:   src/layouts/Layout.astro 	new file:   src/pages/index.astro 	new file:   tailwind.config.cjs 	new file:   tsconfig.json

---
+++ src/components/mform.tsx
@@ -0,0 +1,31 @@
+
+import { useForm } from "react-hook-form";
+
+export default function MForm() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+  const onSubmit = (data) => console.log(data);
+  console.log(errors);
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)} className="sm:space-x-2 flex space-y-2 sm:space-y-0 flex-col sm:flex-row items-center">
+      <input
+        type="text"
+        placeholder="E-Mail"
+        {...register("Email", { required: true, pattern: /^\S+@\S+$/i })}
+        className="border-2 rounded-lg h-12 w-72 text-center"
+      />
+
+      <button
+        type="submit"
+        className=" rounded-lg bg-black text-white h-12 w-36"
+        
+      >
+        Probiere es aus
+      </button>
+    </form>
+  );
+}
