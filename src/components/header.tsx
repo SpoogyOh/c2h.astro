d0a3ce91b0faa639c48bc03f631a5e69c3430422
SpoogyOH <opiola.daniel@gmail.com>
2022-10-30 01:10:16 +0200

    navbar

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -11,16 +11,25 @@ const links = [
 export const MobileHeader = () => {
   const [isOpen, setIsOpen] = useState(false);
 
+  const handleNav = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
-    <div className="fixed text-white left-0 h-full top-0 w-[70%] bg-[#43998e]">
-      <img src="/logow.png" className="w-48 mt-10 pl-8" />
-      <ul className="p-4 pt-20 uppercase text-xl">
-        <li className="p-4 border-b border-gray-100" >Home</li>
-        <li className="p-4 border-b border-gray-100">So funktoniert es</li>
-        <li className="p-4 border-b border-gray-100">Probier es aus</li>
-        <li className="p-4 border-b border-gray-100">Das Team</li>
-        <li className="p-4 border-b border-gray-100">Kontakt</li>
-      </ul>
+    <div>
+      <div className="p-4">
+        <button onClick={handleNav}>BURGER</button>
+      </div>
+      <div className={!isOpen ? "fixed text-white left-0 h-full top-0 w-[70%] bg-[#43998e]" : "hidden"}>
+        <img src="/logow.png" className="w-48 mt-10 pl-8" />
+        <ul className="p-4 pt-20 uppercase text-xl">
+          <li className="p-4 border-b border-gray-100">Home</li>
+          <li className="p-4 border-b border-gray-100">So funktoniert es</li>
+          <li className="p-4 border-b border-gray-100">Probier es aus</li>
+          <li className="p-4 border-b border-gray-100">Das Team</li>
+          <li className="p-4 border-b border-gray-100">Kontakt</li>
+        </ul>
+      </div>
     </div>
   );
 };
