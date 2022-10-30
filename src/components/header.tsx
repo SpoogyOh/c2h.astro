b21ae273fa7118bae8a52696c711b69d43200c50
SpoogyOH <opiola.daniel@gmail.com>
2022-10-30 12:58:08 +0100

    aa

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -28,8 +28,8 @@ export const MobileHeader = () => {
             className="h-8 hover:text-[#f7e07d]"
           >
             <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
+              strokeLinecap="round"
+              strokeLinejoin="round"
               d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
             />
           </svg>
